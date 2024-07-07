e37af9ae297adae20abef36df308556ba6971fcd
Alireza <alirezasaadat056@gmail.com>
2024-07-07 14:09:10 -0700

    bird and movement

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -8,6 +8,30 @@ block_lst = []
 
 counter = 0
 
+class Bird:
+    def __init__(self):
+        self.x = 300
+        self.y = 200
+        
+        self.gravity = 1
+        self.velocity = -10
+    
+        self.surface = pygame.Surface( (40 , 40) )
+        self.surface.fill("red")
+    def jump(self):
+        self.velocity = -15
+        self.y -= 30
+    
+    def update(self):
+        self.velocity += self.gravity
+        self.y += self.velocity
+        
+    def draw(self):
+        self.surface_rect = self.surface.get_rect(center = (self.x , self.y))
+        screen.blit(self.surface , self.surface_rect)
+
+
+bird = Bird()
 class Block:
     def __init__(self , x , y , side):
         self.x = x
@@ -40,9 +64,14 @@ def create_block():
     
     
 def draw():
+    bird.update()
+    bird.draw()
+    
     for block in block_lst:
         block.update()
         block.draw()
+        if block.x < 0 - block.width:
+            block_lst.remove(block)
 
 pygame.init()
 screen = pygame.display.set_mode( (WIDTH , HEIGHT) )
@@ -58,6 +87,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird.jump()
     if counter % 150 == 0:
         create_block()
     draw()
